ee3e7883655f7cdd61e8ac7d06d0604be37a4b37
hongqingbaiji <869227436@qq.com>
2021-05-06 23:45:04 +0800

    0506

--- supermall/src/network/request.js
+++ supermall/src/network/request.js
@@ -3,14 +3,17 @@ import axios from 'axios';
 export function request(config) {
   // 1.创建axios的实例
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000',
+    // baseURL: 'http://123.207.32.32:8000',
+    baseURL: 'http://152.136.185.210:7878/api/m5',
     timeout: 5000
   });
 
   // 2.axios的拦截器
   instance.interceptors.request.use(config => {
     return config
-  }, err => {});
+  }, err => {
+    console.log(err);
+  });
 
   // 响应拦截
   instance.interceptors.response.use(res => {
--- supermall/src/views/home/Home.vue
+++ supermall/src/views/home/Home.vue
@@ -7,6 +7,8 @@
     <home-recommend-view :recommends="recommends"></home-recommend-view>
     <home-feature-view></home-feature-view>
     <tab-control class="tab-control" :titles="['流行','新款','精选']"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
+
     <div class="abc">123</div>
   </div>
 </template>
@@ -14,6 +16,7 @@
 <script>
 import NavBar from 'components/common/navbar/NavBar';
 import TabControl from 'components/content/tabControl/TabControl';
+import GoodsList from 'components/content/goods/GoodsList';
 
 import HomeSwiper from './childComps/HomeSwiper';
 import HomeRecommendView from './childComps/HomeRecommendView';
@@ -28,7 +31,8 @@ export default {
     HomeSwiper,
     HomeRecommendView,
     HomeFeatureView,
-    TabControl
+    TabControl,
+    GoodsList
   },
   data(){
     return{
@@ -36,23 +40,34 @@ export default {
       recommends:[],
       goods:{
         'pop':{page: 0, list:[]},
-        'news':{page: 0, list:[]},
+        'new':{page: 0, list:[]},
         'sell':{page: 0, list:[]},
       }
     }
   },
   created(){
     // 1.请求多个数据
-    getHomeMultidata().then(res=>{
-      // console.log(res.data.banner.list);
-      this.banners = res.data.banner.list;
-      this.recommends = res.data.recommend.list;
-    }) 
+    this.getHomeMultidata();
 
     //2.请求商品数据
-    getHomeGoods('pop',1).then(res=>{
-      console.log(res);
-    })
+    this.getHomeGoods('pop');
+    this.getHomeGoods('new');
+    this.getHomeGoods('sell');
+  },
+  methods:{
+    getHomeMultidata(){
+      getHomeMultidata().then(res=>{
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      }) 
+    },
+    getHomeGoods(type){
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type,page).then(res=>{
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+      })
+    }
   }
 }
 </script>
@@ -73,13 +88,17 @@ export default {
 .home-banners{
   margin-top: 44px;
 }
+
+.tab-control{
+  position: sticky;
+  top:44px;
+  z-index: 9;
+  margin-bottom: 5px;
+}
+
+
 .abc{
   height: 1000px;
   background-color: #ccc;
 }
-.tab-control{
-  position: sticky;
-  top:44px;
-}
-
 </style>
