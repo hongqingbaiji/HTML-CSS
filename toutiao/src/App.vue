b15fe5c975e4adf40a6a61b0d986733d838d64e1
hongqingbaiji <869227436@qq.com>
2021-07-02 09:11:21 +0800

    create toutiao

---
+++ toutiao/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>黑马头条</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+
+</style>
+
