8c85ae7f5fe272b26b610afe45287d6336d360db
hongqingbaiji <869227436@qq.com>
2021-07-13 19:48:46 +0800

    0713

--- toutiao/postcss.config.js
+++ toutiao/postcss.config.js
@@ -1,12 +1,19 @@
 // postcss的配置文件
-// 如果报错或者没有生效，记得将下 postcss-pxtorem 的版本 @5.1.1
+// 如果报错或者没有生效，记得降下 postcss-pxtorem 的版本 @5.1.1
 module.exports = {
+  // 配置要使用的相关插件
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8'],
-    },
+    // 自动添加浏览器厂商声明前缀，用来兼容不同浏览器的
+    // VueCLI内部已经配置过 autoprefixer 把这个配置放到 .browserslistrc 文件中
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8'],
+    // },
     'postcss-pxtorem': {
+      // 转换的根元素基准值
+      // 正常情况下按照设计稿来， 750宽就写75
       rootValue: 37.5,
+
+      // 配置需要转换的 css 属性，有单位的都要转
       propList: ['*'],
     },
   },
--- toutiao/src/App.vue
+++ toutiao/src/App.vue
@@ -6,6 +6,16 @@
     <van-button type="default">默认按钮</van-button>
     <van-button type="warning">警告按钮</van-button>
     <van-button type="danger">危险按钮</van-button>
+    <div>
+      <i class="iconfont icon-pinglun"></i>
+      <i class="iconfont icon-shoucang"></i>
+    </div>
+    <br>
+    <div>
+      <van-icon name="chat-o" dot />
+      <!-- <van-icon name="chat-o" badge="9" />
+      <van-icon name="chat-o" badge="99+" /> -->
+    </div>
     <router-view/>
   </div>
 </template>
