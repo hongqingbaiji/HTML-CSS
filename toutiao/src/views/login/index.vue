eea6b0542a777809ccbad23e81cdae787c7fc7e9
hongqingbaiji <869227436@qq.com>
2021-07-16 09:56:48 +0800

    0716

--- toutiao/src/views/login/index.vue
+++ toutiao/src/views/login/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="login-container">
-    登录页面
+    <van-nav-bar title="登录" left-arrow @click-left="$router.back()" />
+    <van-cell-group>
+      <van-field
+        v-model="user_mobile"
+        left-icon="phone-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user_code"
+        clearable
+        left-icon="comment-o"
+        placeholder="请输入验证码">
+        <template #button>
+          <van-button size="small" round>发送验证码</van-button>
+        </template>
+      </van-field> 
+    </van-cell-group>
+    <van-button type="info" block>登录</van-button>
   </div>
 </template>
 
@@ -9,8 +26,13 @@ export default {
   name: 'LoginIndex',
   data() {
     return {
+      user_mobile:'',
+      user_code:''
     };
   },
+  methods:{
+
+  }
 };
 </script>
 
